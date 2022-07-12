a9bbe0a56f9e8875b368848c02da8457256329ed
João Neto Castro <jneto111@gmail.com>
2022-07-12 20:44:19 -0300

    feat: adiciona main()

--- ED/stack.py
+++ ED/stack.py
@@ -1,88 +1,115 @@
 from datetime import date
 
-
-class Product:
+class Produto:
     def __init__(self, name: str, category: str, expiration: date):
         self.name = name
         self.category = category
         self.expiration = expiration
+
     def __str__(self):
-        return f"{self.name} - {self.category} - {self.expiration}"
+        formated_expiration = self.expiration.strftime("%d/%m/%Y")
+        return f"{self.name} - {self.category} - {formated_expiration}"
 
-class ProductStack:
-    _products = {}
+class PilhasDeProdutos:
+    _pilhas = {}
 
-    def add(self, product: Product):
-        stack = self._products.get(product.category, None)
+    def add(self, product: Produto):
+        pilha = self._pilhas.get(product.category, None)
         temp_stack = []
 
-        if stack is None:
-            print("Inserir produto: ", product)
-            self._products[product.category] = [product]
+        if pilha is None:
+            print("Empilhe o produto: ", product)
+            self._pilhas[product.category] = [product]
             return True
 
-        for i in reversed(range(len(stack))):
-            if product.expiration >= stack[i].expiration:
-                print("Remover produto: ", stack[i])
-                temp_stack.append(stack.pop(i))
+        for i in reversed(range(len(pilha))):
+            if product.expiration > pilha[i].expiration:
+                print("Remova o produto: ", pilha[i])
+                temp_stack.append(pilha.pop(i))
 
-        print("Inserir produto: ", product)
-        stack.append(product)
-        self._products[product.category] = stack + temp_stack
+        print("Empilhe o produto: ", product)
+        pilha.append(product)
+        self._pilhas[product.category] = pilha + temp_stack
 
         for product in reversed(temp_stack):
-            print("Inserir produto: ", product)
-            stack.append(product)
-            self._products[product.category] = stack
+            print("Empilhe o produto: ", product)
+            pilha.append(product)
+            self._pilhas[product.category] = pilha
 
-        
-    
         return True
 
-    def remove(self):
-        return self._products.pop()
+    def remove(self, category):
+        pilha = self._pilhas.get(category, [])
+        if len(pilha) == 0:
+            return None
+        return pilha.pop()
 
     def show(self):
-        for category, products in self._products.items():
-            print(f"{category}:")
-            for product in products:
-                print("\t", product)
+        for category, products in self._pilhas.items():
+            print("\n---------------------------------------\n")
+            print("Categoria: {}".format(category))
+            for product in reversed(products):
+                print("    {}".format(product))
             print("\n")
+        print("---------------------------------------\n")
 
 
 def menu():
     print("""
-    1 - Cadastrar produto
-    2 - Listar produtos
-    3 - Alterar produto
-    4 - Excluir produto
-    5 - Sair
+---------------------------------------
+
+1 - Cadastrar produto
+2 - Listar produtos
+3 - Tirar produto do estoque
+0 - Sair
+ 
+---------------------------------------
     """)
     return input("Digite a opção desejada: ")
     
-print("Bem vindo ao sistema de controle de estoque")
+def main():
+    print("Bem vindo ao sistema de controle de estoque")
 
-stack = ProductStack()
+    pilha = PilhasDeProdutos()
 
-product1 = Product("Produto 1", "arroz", date(2020, 1, 1))
-product2 = Product("Produto 2", "arroz", date(2020, 1, 2))
-product3 = Product("Produto 3", "arroz", date(2020, 1, 3))
-product4 = Product("Produto 4", "arroz", date(2020, 1, 4))
+    # produto1 = Produto("Produto 1", "arroz", date(2020, 1, 1))
+    # produto2 = Produto("Produto 2", "arroz", date(2020, 1, 2))
+    # produto3 = Produto("Produto 3", "arroz", date(2020, 1, 3))
+    # produto4 = Produto("Produto 4", "arroz", date(2020, 1, 4))
 
-stack.add(product3)
-stack.add(product2)
-stack.add(product1)
-stack.add(product4)
+    # pilha.add(produto3)
+    # pilha.add(produto2)
+    # pilha.add(produto1)
+    # pilha.add(produto4)
 
-stack.show()
+    # pilha.show()
+    # return
+    while True:
+        option = menu()
 
-# while True:
-#   option = menu()
+        if option == "1":
+            print("\n--------- Cadastrando Produto ---------\n")
 
-#   if option == "1":
-#     print("Cadastrar produto")
-#     name = input("Nome: ")
-#     category = input("Categoria: ")
-#     expiration = input("Data de validade: ")
-#     product = Product(name, category, expiration)
-#     stack.add(product)
+            name = input("Nome: ")
+            category = input("Categoria: ").lower()
+            [day, month, year] = input("Data de validade (dd/mm/YYYY): ").split("/")
+            product = Produto(name, category, date(int(year), int(month), int(day)))
+            pilha.add(product)
+            print("\n---------------------------------------\n")
+
+        elif option == "2":
+            pilha.show()
+
+        elif option == "3":
+            category = input()
+            produto = pilha.remove(category)
+            if produto:
+                print(produto)
+            else:
+                print("Essa categoria não tem produtos")
+
+        elif option == "0":
+            return
+
+if __name__ == "__main__":
+    main()
